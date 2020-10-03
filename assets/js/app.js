d5bfebd14a6ab6fba6b92c6675f8ea5c3e827130
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2020-10-03 16:38:41 +0530

    Added local storage and structured code

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,6 +15,18 @@ function toggleMode() {
 
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("timeDial").clientHeight = document.getElementById("sunCard").clientHeight;
+
+  const qcity = localStorage.getItem("city");
+  if (qcity) {
+    weather = currWeather.getWeather(qcity).then((data) => {
+      console.log(data);
+      if (data.cod !== "404") {
+        buildDOM(data);
+      } else {
+        document.getElementById("cityName").innerHTML = "Invalid City!";
+      }
+    });
+  }
 });
 
 document.getElementById("subBtn").addEventListener("click", () => {
@@ -23,9 +35,16 @@ document.getElementById("subBtn").addEventListener("click", () => {
     console.log(city);
     weather = currWeather.getWeather(city).then((data) => {
       console.log(data);
-      buildDOM(data);
+      if (data.cod !== "404") {
+        buildDOM(data);
+      } else {
+        document.getElementById("cityName").innerHTML = "Invalid City!";
+      }
     });
+
     city.value = "";
+
+    localStorage.setItem("city", city);
   }
 });
 
@@ -63,53 +82,4 @@ function buildDOM(weatherData) {
     weatherData.sys.sunset + weatherData.timezone
   );
   document.getElementById("cityName").innerHTML = `${weatherData.name}, ${weatherData.sys.country}`;
-}
-
-function kelvinToCelsius(temp) {
-  return Math.round(temp - 273.15);
-}
-
-function weatherIcon(iconId) {
-  switch (iconId) {
-    case "01d":
-      return "wi-day-sunny";
-    case "01n":
-      return "wi-night-clear";
-
-    case "02d":
-      return "wi-day-cloudy";
-    case "02n":
-      return "wi-night-alt-cloudy";
-
-    case "03d":
-    case "03n":
-      return "wi-cloud";
-
-    case "04d":
-    case "04n":
-      return "wi-cloudy";
-
-    case "09d":
-      return "wi-day-showers";
-    case "09n":
-      return "wi-night-alt-showers";
-
-    case "10d":
-      return "wi-rain";
-    case "10n":
-      return "wi-night-alt-rain";
-
-    case "11d":
-      return "wi-day-thunderstorm";
-    case "11n":
-      return "wi-night-thunderstorm";
-
-    case "13d":
-    case "13n":
-      return "wi-snow";
-
-    case "50d":
-    case "50n":
-      return "wi-windy";
-  }
 }
--- assets/js/weather.js
+++ assets/js/weather.js
@@ -13,3 +13,52 @@ class Weather {
     return weather;
   }
 }
+
+function kelvinToCelsius(temp) {
+  return Math.round(temp - 273.15);
+}
+
+function weatherIcon(iconId) {
+  switch (iconId) {
+    case "01d":
+      return "wi-day-sunny";
+    case "01n":
+      return "wi-night-clear";
+
+    case "02d":
+      return "wi-day-cloudy";
+    case "02n":
+      return "wi-night-alt-cloudy";
+
+    case "03d":
+    case "03n":
+      return "wi-cloud";
+
+    case "04d":
+    case "04n":
+      return "wi-cloudy";
+
+    case "09d":
+      return "wi-day-showers";
+    case "09n":
+      return "wi-night-alt-showers";
+
+    case "10d":
+      return "wi-rain";
+    case "10n":
+      return "wi-night-alt-rain";
+
+    case "11d":
+      return "wi-day-thunderstorm";
+    case "11n":
+      return "wi-night-thunderstorm";
+
+    case "13d":
+    case "13n":
+      return "wi-snow";
+
+    case "50d":
+    case "50n":
+      return "wi-windy";
+  }
+}
